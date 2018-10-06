3d609f48c1bd8d23cf141e8c6f7e1f0e09d4370d
Jan Czekawski <jan.czekawski@interia.pl>
2018-10-06 19:23:22 +0200

    Update 6 exercise in the basics.

--- introduction_to_ruby_programming_book/2-the-basics/6-exercise.rb
+++ introduction_to_ruby_programming_book/2-the-basics/6-exercise.rb
@@ -1,5 +1,5 @@
 # Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen.
 
 puts 3.5 * 3.5
-puts 4.11 * 4.11
+puts 4.11**2
 puts 11.3 * 11.3
