a3a6c8f7de3cb28e7318dc8f3b73b09fa363129c
Jan Czekawski <jan.czekawski@interia.pl>
2018-10-06 18:16:24 +0200

    Add exercises to getting started chapter

---
+++ introduction_to_ruby_programming_book/1-getting-started/2-exercise.rb
@@ -0,0 +1,6 @@
+# When you are finished with the above and both programs are working correctly, navigate to the directory above the my_folder directory and delete all of the content you generated with one command.
+
+# Warning: The rm method is a DANGEROUS command. Once you run this command there is no undoing it. It is permanent. Make sure you are in the correct directory and you are referencing the correct target directory when you execute this command.
+
+cd ..
+rm -r my_folder
